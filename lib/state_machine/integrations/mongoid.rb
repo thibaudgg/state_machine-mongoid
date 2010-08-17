171b9e0a493d857c8d0826207263bbf539331920
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-08-17 16:06:43 +0200

    Back to simple base def

--- lib/state_machine/integrations/mongoid.rb
+++ lib/state_machine/integrations/mongoid.rb
@@ -8,30 +8,6 @@ module StateMachine::Integrations::Mongoid
   end
   
 protected
-  # Whether observers are supported in the integration.  Only true if
-  # ActiveModel::Observer is available.
-  def supports_observers?
-    defined?(::ActiveModel::Observing) && owner_class <= ::ActiveModel::Observing
-  end
-  
-  # Whether validations are supported in the integration.  Only true if
-  # the ActiveModel feature is enabled on the owner class.
-  def supports_validations?
-    defined?(::ActiveModel::Validations) && owner_class <= ::ActiveModel::Validations
-  end
-  
-  # Do validations run when the action configured this machine is
-  # invoked?  This is used to determine whether to fire off attribute-based
-  # event transitions when the action is run.
-  def runs_validations_on_action?
-    action == :save
-  end
-  
-  # Whether change (dirty) tracking is supported in the integration.
-  # Only true if the ActiveModel feature is enabled on the owner class.
-  def supports_dirty_tracking?(object)
-    defined?(::ActiveModel::Dirty) && owner_class <= ::ActiveModel::Dirty && object.respond_to?("#{self.attribute}_changed?")
-  end
   
   # Defines an initialization hook into the owner class for setting the
   # initial state of the machine *before* any attributes are set on the
